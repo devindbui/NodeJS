0c7ec9071a2d08feb288ccdecf32b5b7db1615c2
Devin <Devin@Win10-VM3-1>
2018-06-02 11:43:03 -0700

    added url

--- server.js
+++ server.js
@@ -3,3 +3,5 @@ var server = http.createServer(function(request,response){
 	response.writeHead(200, {"Content-Type": "text/html"});
 	response.end("Hello Devin");
 }).listen(8888);
+
+//http://localhost:8888/
